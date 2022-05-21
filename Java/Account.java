e3d89804563c978918444dd892d18ea7fb106d1b
Luis Diaz <luisdiazdevo@gmail.com>
2022-05-20 23:34:49 -0500

    Se crean clases en Java y Phyton

---
+++ Java/Account.java
@@ -0,0 +1,11 @@
+package Java;
+
+class Account {
+    Integer id;
+    String name;
+    String document;
+    String emai;
+    String password;
+}
+
+
---
+++ Java/Car.java
@@ -0,0 +1,8 @@
+package Java;
+
+class Car {
+    Integer id;
+    String licence;
+    String driver;
+    Integer passegenger;
+}
