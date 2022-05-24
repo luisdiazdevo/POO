40d09e5a964d9c021e174ea70981544744c638c2
Luis Diaz <luisdiazdevo@gmail.com>
2022-05-24 14:11:23 -0500

    Se cambian los metodos contructores en JavaScript y se coloca el nuevo metodo CLASS

--- JS/Account.js
+++ JS/Account.js
@@ -1,8 +1,19 @@
-function Account (name, document){
-    this.id;
-    this.name = name;
-    this.document = document;
-    this.email;
-    this.password;
-}
+class Account {
 
+    constructor(name, document){
+        this.id;
+        this.name = name;
+        this.document = document;
+        this.email;
+        this.password;
+    }
+
+
+    printDataCar() {
+        document.getElementById("name").innerHTML="Nombre: "+this.driver.name
+        document.getElementById("document").innerHTML="Tu Documento que subiste: "+this.driver.document
+        document.getElementById("passangers").innerHTML="Maximo de pasajeros: "+this.passanger
+        document.getElementById("matricula").innerHTML="Matricula del auto: "+this.license
+    }
+
+}
--- JS/Car.js
+++ JS/Car.js
@@ -1,19 +1,17 @@
-function Car (license, driver, passanger){
-    this.id;
-    this.license = license;
-    this.driver = driver;
-    this.passanger = passanger;
-}
+class Car {
 
-Car.prototype.printDataCar = function () {
-    console.log(this.driver)
-    console.log(this.driver.name)
-    console.log(this.driver.document)
-    console.log(this.passanger)
-    console.log(this.license)
-    document.getElementById("name").innerHTML="Nombre: "+this.driver.name
-    document.getElementById("document").innerHTML="Tu Documento que subiste: "+this.driver.document
-    document.getElementById("passangers").innerHTML="Maximo de pasajeros: "+this.passanger
-    document.getElementById("matricula").innerHTML="Matricula del auto: "+this.license
+    constructor(license, driver, passanger){
+        this.id;
+        this.license = license;
+        this.driver = driver;
+        this.passanger = passanger;
+    }
+
+    printDataCar(){
+        document.getElementById("name").innerHTML="Nombre: "+this.driver.name
+        document.getElementById("document").innerHTML="Tu Documento que subiste: "+this.driver.document
+        document.getElementById("passangers").innerHTML="Maximo de pasajeros: "+this.passanger
+        document.getElementById("matricula").innerHTML="Matricula del auto: "+this.license
+    }
 
 }
